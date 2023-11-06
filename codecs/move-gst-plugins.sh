7cee5da81bba7c5722c7a49656e91c50aadcb73f
Will Thompson <will@willthompson.co.uk>
2023-11-06 20:00:48 +0000

    Keep -bad plugins in app if in fd.o runtime
    Previously, all plugins from gst-plugins-bad were exiled to the .Codecs
    extension, including many that are already part of the runtime.
    
    Instead, include any plugin which is present in the runtime in the main
    app – this doesn't increase its exposure to any patent issues because
    all the code in question is present anyway. A few plugins from -bad
    are shipped in .Codecs but not in the runtime, such as DVD support and
    v4l2 accelerated codec support – leave these where they are.
    
    While we're here, clean up some shellcheck warnings in the splitting
    script. The use of '==' in a [ test already meant a dependency on bash;
    make this explicit so we can use process substitution. Rewrite the
    gst-inspect-1.0 parsing to allow for better logging. The script is now
    clean with shellcheck 0.9.0.
    
    See https://github.com/flathub/org.gnome.Totem.Devel/issues/25

--- codecs/move-gst-plugins.sh
+++ codecs/move-gst-plugins.sh
@@ -1,15 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
 mkdir -p /app/lib/codecs/lib/gstreamer-1.0
+
+SYSTEM_MODULES_DIR=$(PKG_CONFIG_PATH='' pkg-config --variable=pluginsdir gstreamer-1.0)
 export GST_PLUGIN_SYSTEM_PATH=/app/lib/gstreamer-1.0
 export LD_LIBRARY_PATH=/app/lib/codecs/lib/:/app/lib/
 
 for i in /app/lib/gstreamer-1.0/*.so; do
-  SUITE=`gst-inspect-1.0 $i | grep "Source module" | sed 's,Source module,,' | tr -d [:blank:]`
-  if [ x$SUITE == x'gst-plugins-bad' ] ||
-     [ x$SUITE == x'gst-plugins-ugly' ] ||
-     [ x$SUITE == x'gst-libav' ]; then
-    mv $i /app/lib/codecs/lib/gstreamer-1.0
-  fi
-done
+  while read -r line
+  do
+    case "$line" in
+      "Source module"*)
+        SUITE="$(echo "$line" | sed 's,Source module,,' | tr -d '[:blank:]')"
+        ;;
+      Name*)
+        PLUGIN="$(echo "$line" | sed 's,Name,,' | tr -d '[:blank:]')"
+        ;;
+    esac
+  done < <(gst-inspect-1.0 "$i")
 
+
+  case "$SUITE" in
+    gst-plugins-ugly|gst-libav)
+      echo "Exiling $PLUGIN from $SUITE to codecs runtime" >&2
+      mv "$i" /app/lib/codecs/lib/gstreamer-1.0
+      ;;
+
+    gst-plugins-bad)
+      if [ -e "$SYSTEM_MODULES_DIR/$(basename "$i")" ]
+      then
+        echo "Keeping $PLUGIN from $SUITE (present in runtime) in app" >&2
+      else
+        echo "Exiling $PLUGIN from $SUITE to codecs runtime" >&2
+        mv "$i" /app/lib/codecs/lib/gstreamer-1.0
+      fi
+      ;;
+  esac
+done
